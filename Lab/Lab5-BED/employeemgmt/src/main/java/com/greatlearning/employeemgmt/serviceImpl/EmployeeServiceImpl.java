b3e34890a4104fa220328926ab5517d35249dd98
santoshtuniki <tunikisantosh123@gmail.com>
2023-01-23 17:35:29 +0530

    Springboot added

---
+++ Lab/Lab5-BED/employeemgmt/src/main/java/com/greatlearning/employeemgmt/serviceImpl/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.greatlearning.employeemgmt.serviceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.greatlearning.employeemgmt.Repository.EmployeeRepository;
+import com.greatlearning.employeemgmt.model.Employee;
+import com.greatlearning.employeemgmt.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Override
+	public List<Employee> getAll() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public void saveOrUpdate(Employee employee) {
+		employeeRepository.save(employee);
+		employeeRepository.flush();
+	}
+
+	@Override
+	public void deleteEmployeeById(int id) {
+		employeeRepository.deleteById(id);
+	}
+
+	@Override
+	public Employee getEmployeeById(int id) {
+		Optional<Employee> result = employeeRepository.findById(id);
+		if (result.isPresent()) {
+			return result.get();
+		} else {
+			throw new RuntimeException("Employee does not exists for the Id: " + id);
+		}
+	}
+
+}
